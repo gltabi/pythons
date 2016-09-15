da85fa6b028a28afc946b9e8737fa0a896e76ec2
gltabi <tabii12@gmail.com>
2016-09-15 12:12:43 -0700

    Update control.py

--- REMOTE CONTROL/control.py
+++ REMOTE CONTROL/control.py
@@ -1,10 +1,37 @@
 import key_mapping
 import os
 import time
-import Image
+#import Image
 
-image = Image.open('arrisremote_labelled.jpg')
-image.show()
+#image = Image.open('arrisremote_labelled.jpg')
+#image.show()
 
+print('Input a sequence to be transmitted')
+print('Please enter milliseconds between each command')
+timeBetween = raw_input()
+print(timeBetween + 'millisecond delay')
+timeBetween = float(timeBetween)
+
+print('Command numbers are labelled according to the image')
+
+repeat == 'n'
+while(repeat == 'n'):
+    print('Please enter the command numbers separated by a space)
+    command = raw_input()
+    print(command + 'entered')
+    command = command.split(' ')
+
+    arraySize = len(command)
+
+    repeat = 'y';
+
+    while(repeat == 'y'):
+        for i in range(arraySize)
+            time.sleep(timeBetween)
+            toEnter = ('irsend SEND_ONCE' + getRemoteName(command[i]) + keys[command[i]])   
+            os.system(toEnter)
+
+        print('repeat sequence? y/n')
+        repeat = raw_input()
 
 
